262c31c48202a678f8f63ea9670a68ec12601826
alfredp77 <alfred_scb@yahoo.com.sg>
2019-05-16 21:38:49 +0800

    very basic starting page

---
+++ busfin/src/LauncherEntry.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { Launcher } from './components/Launcher';
+
+
+ReactDOM.render(<Launcher />, document.getElementById('root'));
---
+++ busfin/src/components/Launcher.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+export const Launcher = () => {    
+    return (
+      <div className="hello">
+        <div className="greeting">
+          Hello World from Busfin!
+        </div>
+      </div>
+    );
+}
