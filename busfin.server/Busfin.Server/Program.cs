52544f67475484bc2d19794964f9e69eac60b0ff
Alfred Padawangi <alfredp77@outlook.com>
2019-06-02 21:22:42 +0800

    busfin.server

---
+++ busfin.server/Busfin.Server/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace Busfin.Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appconfig.json")
+                .Build();
+
+            var webHost = new WebHostBuilder()
+                .UseKestrel()
+                .UseWebRoot("dist")
+                .UseUrls($"http://localhost:{config["Port"]}")
+                .UseStartup<Program>()
+                .Build();
+            webHost.Run();
+
+        }
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            app.UseStaticFiles();
+        }
+
+    }
+}
